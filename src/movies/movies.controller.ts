df2a6ee4079d66698164335a76a1dfdbb1cd22f3
alisemagatroid <csw2479@gmail.com>
2023-08-05 17:07:10 +0900

    네스트 튜토리얼

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Delete, Get, Param, Post, Patch, Body, Query, Res, Req } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+@Controller('movies')
+export class MoviesController {
+
+    constructor(private readonly moviesService: MoviesService) {}
+
+    @Get()
+    getAll(@Req() req, @Res() res): Movie[] {
+        return this.moviesService.getAll();
+    }
+
+    @Get(":id")
+    getOne(@Param("id") movieID: number): Movie{
+        return this.moviesService.getOne(movieID);
+    }
+
+    @Post()
+    create(@Body() movieData: CreateMovieDto) {
+        return this.moviesService.create(movieData);
+    }
+
+    @Delete(":id")
+    remove(@Param("id") movieID: number) {
+        return this.moviesService.deleteOne(movieID)
+    }
+
+    @Patch(":id")
+    patch(@Param("id") movieID: number, @Body() updateData: UpdateMovieDto) {
+        return this.moviesService.update(movieID, updateData)
+    }
+}
